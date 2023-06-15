e1a13999fd29bec9d258dc8b10bf146363719468
Pavel Uzuniants <holabeda@gmail.com>
2023-06-15 18:12:21 +0300

    fix: пофиксил небольшой баг с env зависимостями

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { limiter } from "./utils/rateLimiter.js";
-import { PORT, DATA_BASE, MONGODB_URL } from "./config.js";
+import { PORT, DATA_BASE } from "./config.js";
 import { router } from "./routers/index.js";
 import { corsOptions } from "./middlewares/corsOptions.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+const MONGODB_URL = process.env.MONGODB_URL;
 const app = express();
 
 mongoose.connect(MONGODB_URL);
--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const { PORT = 3001 } = process.env;
-const { MONGODB_URL } = process.env;
+const MONGODB_URL = process.env.MONGODB_URL;
 const DATA_BASE = "mongodb://localhost:27017";
 
 export { PORT, DATA_BASE, MONGODB_URL };
